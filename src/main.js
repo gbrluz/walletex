81738021ef8658507bf87e1eb1e11af86e6b7124
Gabriel <gabriel-h-luz@hotmail.com>
2024-04-24 15:45:04 -0300

    Revert "adjust"
    This reverts commit ebf6a416c2da6ef0d55a8a2520776dcdbe4815da.

--- src/main.js
+++ src/main.js
@@ -25,5 +25,6 @@ const myApp = createApp({
 
 myApp.use(Quasar).use(router)
 
+// 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
 // Assumes you have a <div id="app"></div> in your index.html
 myApp.mount('#app')
